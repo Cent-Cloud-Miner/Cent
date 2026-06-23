df10ab90e1e8866023bb8bff94981a44b12f112d
KingricharDV#0439 <kingrichardv@protonmail.com>
2026-06-23 18:17:13 +0900

    updated files

--- src/amount.h
+++ src/amount.h
@@ -14,7 +14,7 @@
 typedef int64_t CAmount;
 
 static const CAmount COIN = 100000000;
-static const CAmount CENT = 1000000;
+static const CAmount CENT = 100000000;
 
 /** Type-safe wrapper class to for fee rates
  * (how much to pay based on transaction size)
