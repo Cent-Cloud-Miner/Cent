48e66acec5792dc11d1e12f7cdeae3744eaca70b
KingricharDV#0439 <kingrichardv@protonmail.com>
2026-08-11 08:57:58 +0900

    Update 2MN.sh

--- scripts/2MN.sh
+++ scripts/2MN.sh
@@ -51,7 +51,7 @@ sudo echo ' /var/swap.img none swap sw 0 0 ' >> /etc/fstab
 cd ~ 
 sudo mkdir -p /root/cent/home/taihei/Cent/src/
 cd /root/cent
-wget https://github.com/Cent-Cloud-Miner/Cent/releases/download/1.0.0.2/centlinux.tar.gz
+wget https://github.com/Cent-Cloud-Miner/Cent/releases/download/1.0.2.1.0.0.3/centlinux.tar.gz
 tar -xvzf centlinux.tar -C /root/cent
 sudo mv /root/cent/home/taihei/Cent/src/centd /root/cent/home/taihei/Cent/src/cent-cli /root/cent/home/taihei/Cent/src/cent-tx /root/cent
 
